ae83fe1f53622dfcf5013887eabb2a12e8120675
Wojciech Haniewski <w.haniewski@gmail.com>
2019-01-25 12:37:06 +0100

    "Created spinner"

--- app/src/main/java/com/github/wojhan/epfuel/Vehicle.java
+++ app/src/main/java/com/github/wojhan/epfuel/Vehicle.java
@@ -1,4 +1,31 @@
 package com.github.wojhan.epfuel;
 
 public class Vehicle {
+
+    private String name;
+    private int image;
+
+    public Vehicle () {
+
+    }
+
+    public Vehicle (String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getImage() {
+        return image;
+    }
+
+    public void setImage(int image) {
+        this.image = image;
+    }
 }
--- app/src/main/java/com/github/wojhan/epfuel/VehicleAdapter.java
+++ app/src/main/java/com/github/wojhan/epfuel/VehicleAdapter.java
@@ -1,4 +1,62 @@
 package com.github.wojhan.epfuel;
 
-public class VehicleAdapter {
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.TextView;
+
+import java.util.List;
+
+public class VehicleAdapter extends BaseAdapter {
+
+    private List<Vehicle> mVehicleList;
+
+    public VehicleAdapter() {
+
+    }
+
+    public VehicleAdapter(List<Vehicle> vehicleList) {
+        mVehicleList = vehicleList;
+    }
+
+    @Override
+    public int getCount() {
+        return mVehicleList.size();
+    }
+
+    @Override
+    public Object getItem(int position) {
+        return mVehicleList.get(position);
+    }
+
+    @Override
+    public long getItemId(int position) {
+        return position;
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+
+        VehicleHolder holder;
+
+        if (convertView == null) {
+            convertView = LayoutInflater.from(parent.getContext()).inflate(R.layout.row_vehicle_spinner, parent, false);
+
+            holder = new VehicleHolder();
+            holder.mName = convertView.findViewById(R.id.vehicle_spinner_name);
+            convertView.setTag(holder);
+        } else {
+            holder = (VehicleHolder)convertView.getTag();
+        }
+
+        Vehicle vehicle = mVehicleList.get(position);
+        holder.mName.setText(vehicle.getName());
+
+        return convertView;
+    }
+
+    class VehicleHolder {
+        private TextView mName;
+    }
 }
